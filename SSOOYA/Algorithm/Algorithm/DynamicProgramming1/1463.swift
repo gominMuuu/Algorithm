b4a7e23b2256f4430bf50dedcf16f600a76ff38e
SSOOYA <bsy24660@naver.com>
2023-02-06 10:24:31 +0900

    solve/#52: 백준 1463번 - bottomup 풀이

--- SSOOYA/Algorithm/Algorithm/DynamicProgramming1/1463.swift
+++ SSOOYA/Algorithm/Algorithm/DynamicProgramming1/1463.swift
@@ -10,6 +10,7 @@
 import Foundation
 
 let n = Int(readLine()!)!
+
 var storage = [Int:Int]()
 storage[1] = 0
 
@@ -31,6 +32,31 @@ func topdown(_ n: Int) -> Int{
     
     return storage[n]!
 }
-
-
 print(topdown(n))
+
+var stor = [Int](repeating: 0, count: n+1)
+stor[1] = 0
+func bottomup(){
+    
+    if(n == 1){
+        print(0)
+        exit(0)
+    }
+    for i in 2...n{
+        if(i < 4){
+            stor[i] = 1
+            continue
+        }
+        
+        stor[i] = stor[i-1] + 1
+        if(i % 3 == 0){
+            stor[i] = min(stor[i/3] + 1, stor[i])
+        }
+        if(i % 2 == 0){
+            stor[i] = min(stor[i/2] + 1, stor[i])
+        }
+    }
+    print(stor[n])
+}
+
+bottomup()
