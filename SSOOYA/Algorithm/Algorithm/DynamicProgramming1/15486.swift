8e122fa207ebd654a1ff7dfde2773ede09f482a4
SSOOYA <bsy24660@naver.com>
2023-02-08 20:07:46 +0900

    solve/#56: 백준 15486번

--- SSOOYA/Algorithm/Algorithm/DynamicProgramming1/15486.swift
+++ SSOOYA/Algorithm/Algorithm/DynamicProgramming1/15486.swift
@@ -11,23 +11,28 @@ import Foundation
 
 let n = Int(readLine()!)!
 
-var necessaryDate = [Int](repeating: 0, count: n+1)
-var profit = [Int](repeating: 0, count: n+1)
+var necessaryDate = [Int](repeating: 0, count: n+2)
+var profit = [Int](repeating: 0, count: n+2)
 for i in 1...n{
     let info = readLine()!.split(separator: " ").map({ Int($0)! })
     necessaryDate[i] = info[0]
     profit[i] = info[1]
 }
 
-var dp = [Int](repeating: 0, count: n+1)
-for i in 1...n{
-    //상담 마지막 날 기준으로
-    let endDate = i + necessaryDate[i] - 1
-    if(endDate == i && dp[i] != 0){
-        dp[i] = max(dp[i], profit[i])
-    }else if(endDate <= n){
-        dp[endDate] = max(dp[endDate], dp[i - 1] + profit[i])
+var dp = [Int](repeating: 0, count: n+2)
+var result = 0
+
+//일을 마치는 당일을 기준으로 하니까 조건이 많아져서 코드가 복잡해짐
+//일을 마친 다음 날을 기준으로 설정하니까 위의 문제 해결
+for i in 1...n+1{
+    
+    result = max(result, dp[i])
+    
+    let endDate = i + necessaryDate[i]
+    if(endDate > n + 1){
+        continue
     }
-    dp[i] = max(dp[i-1], dp[i])
+    
+    dp[endDate] = max(result + profit[i], dp[endDate])
 }
-print(dp.last!)
+print(result)
