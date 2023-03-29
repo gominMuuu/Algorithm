ad1c933b924e9e502dccd1f4cf792bc9e2320f38
SSOOYA <bsy24660@naver.com>
2023-03-29 09:41:58 +0900

    solve/#96: 백준 2470번

---
+++ SSOOYA/Algorithm/Algorithm/BinarySearch/2470.swift
@@ -0,0 +1,42 @@
+//
+//  2470.swift
+//  Algorithm
+//
+//  Created by 박소윤 on 2023/03/29.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let data = readLine()!.split(separator: " ").map{ Int($0)! }.sorted(by: <)
+var ans = (Int.max / 2, Int.max / 2)
+
+for (index,i) in data.enumerated(){
+    
+    let target = -i
+    var current = Int.max / 2
+    func binarySearch(left: Int, right: Int){
+        if(left > right){
+            return
+        }
+        let mid = (left + right) / 2
+        if(mid != index && (abs(target - data[mid]) < abs(target - current))){
+            current = data[mid]
+        }
+
+        if(data[mid] == target){
+            ans = (min(i, data[mid]), max(i, data[mid])); return
+        }else if(data[mid] > target){
+            binarySearch(left: left, right: mid-1)
+        }else{
+            binarySearch(left: mid+1, right: right)
+        }
+    }
+    
+    binarySearch(left: data.startIndex, right: data.count - 1)
+    if(abs(ans.0 + ans.1) > abs(i + current)){
+        ans = (min(i, current), max(i, current))
+    }
+}
+
+print(ans.0, ans.1)
