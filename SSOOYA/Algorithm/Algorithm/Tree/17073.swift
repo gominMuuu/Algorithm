c2ea7203f5cd359c26efaf9c7197907ea232d31a
SSOOYA <bsy24660@naver.com>
2023-05-16 09:13:52 +0900

    unsolve/#129: 백준 17073번 - 시간 초과

--- SSOOYA/Algorithm/Algorithm/Tree/17073.swift
+++ SSOOYA/Algorithm/Algorithm/Tree/17073.swift
@@ -17,24 +17,10 @@ for _ in 1..<n{
     links[link[1]].append(link[0])
 }
 
-var visit = [Bool](repeating: false, count: n+1)
-var waterAmount = [Int]()
-func fallDown(node: Int, water: Int){
-    visit[node] = true
-    let child = links[node].filter{ !visit[$0] }
-    if child.isEmpty {
-        waterAmount.append(water)
-    } else {
-        let perAmount = water / child.count
-        for (index, id) in child.enumerated(){
-            if index != 0 && index == child.count - 1 {
-                fallDown(node: id, water: water - (child.count - 1) * perAmount)
-            } else {
-                fallDown(node: id, water: perAmount)
-            }
-        }
+var ans = 0
+for (id, childs) in links.enumerated(){
+    if id != 1 && childs.count == 1 {
+        ans += 1
     }
 }
-
-fallDown(node: 1, water: w)
-print(Double(w) / Double(waterAmount.count))
+print(Double(w) / Double(ans))
